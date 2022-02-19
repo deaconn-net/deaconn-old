3d87591ee7d56d93a3617515239d20d2218dec3e
Christian Deacon <gamemann@gflclan.com>
2022-02-19 22:51:09 +0000

    Add Static root.

--- deaconn/deaconn/settings.py
+++ deaconn/deaconn/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = "/deaconn/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
