b00af603c111057d38fe07d72fe07171bb1df656
Christian Deacon <gamemann@gflclan.com>
2022-02-20 18:42:01 +0000

    Try fixing bug.

--- deaconn/home/views.py
+++ deaconn/home/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 def home_view(request):
     info = {}
+    info["gh_stars"] = 243
+    info["gh_repos"] = 99
+    info["gh_followers"] = 2302
+    info["gh_commits"] = 5034
     return render(request, 'home/home.html', info)
