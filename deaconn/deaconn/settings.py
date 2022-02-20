fbd08bdbfc77ce5f581bc736b736ce7f61b38076
Christian Deacon <gamemann@gflclan.com>
2022-02-20 01:28:06 +0000

    Update CSS/HTML.

--- deaconn/deaconn/settings.py
+++ deaconn/deaconn/settings.py
@@ -24,9 +24,9 @@ import os
 SECRET_KEY = 'django-insecure-95-2-^@c))4#_to3*!qz550gr!lf7w=i7k*dgwyhb-pz)m!6ho'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['51.81.86.146', '127.0.0.1', 'localhost', 'deaconn.net']
+ALLOWED_HOSTS = ['51.81.86.146', '127.0.0.1', 'localhost', 'deaconn.net', 'pb-dev01.deacon.internal']
 
 
 # Application definition
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'deaconn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
