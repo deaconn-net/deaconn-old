d227d79f70ce56fe4bc9d62d84d9fec139354cbb
Christian Deacon <gamemann@gflclan.com>
2022-02-23 05:42:03 +0000

    Add code highlighting, etc.

--- deaconn/blog/templatetags/markdown.py
+++ deaconn/blog/templatetags/markdown.py
@@ -6,6 +6,6 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 def do_markdown(contents):
-    return mark_safe(markdown.markdown(contents))
+    return mark_safe(markdown.markdown(contents, extensions=['fenced_code', 'codehilite']))
 
 register.filter('do_markdown', do_markdown)
